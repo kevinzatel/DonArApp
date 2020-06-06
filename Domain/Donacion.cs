559e8380c9256f9badf217eadf286e6d67019abe
Franco <franko.gb@gmail.com>
2020-06-06 15:06:30 -0300

    creacion de clases

---
+++ Domain/Donacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    class Donacion
+    {
+        public int Id { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaEgreso { get; set; }
+        public List<ItemDonacion> Items { get; set; }
+    }
+}
