bc1f06879b32a092adad9937207b8787479347b0
Kevin Zatel <Kevin Zatel@SVARLP2018460>
2020-06-20 14:31:21 -0300

    rango etario reporte

--- API/Controllers/ReportesController.cs
+++ API/Controllers/ReportesController.cs
@@ -18,17 +18,19 @@ namespace API.Controllers
         private readonly IVoluntarioBasicoService _voluntarioService;
         private readonly IVoluntarioMedicoService _voluntarioMedicoService;
         private readonly IVoluntarioAsociacionService _voluntarioAsociacionService;
+        private readonly IPacienteService _pacientenService;
 
-        public ReportesController(IVoluntarioBasicoService voluntarioService, IVoluntarioMedicoService voluntarioMedicoService, IVoluntarioAsociacionService voluntarioAsociacionService)
+        public ReportesController(IVoluntarioBasicoService voluntarioService, IVoluntarioMedicoService voluntarioMedicoService, IVoluntarioAsociacionService voluntarioAsociacionService, IPacienteService pacienteService)
         {
             _voluntarioService = voluntarioService;
             _voluntarioMedicoService = voluntarioMedicoService;
             _voluntarioAsociacionService = voluntarioAsociacionService;
+            _pacientenService = pacienteService;
         }
 
         [Route("voluntarios")]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<VoluntarioBasico>>> Get()
+        public async Task<ActionResult<ReporteVoluntarios>> GetReporteVoluntarios()
         
         {
             var reporteVoluntarios = new ReporteVoluntarios();
@@ -39,6 +41,25 @@ namespace API.Controllers
             reporteVoluntarios.CantVoluntariosMedicos = voluntariosMedicos.Count();
             reporteVoluntarios.CantVoluntariosAsociacion = voluntariosAsociacion.Count();
             return Ok(reporteVoluntarios);
+        }
+
+
+        // Devuelve un array de 6 posiciones que representan los siguientes rangos de edades: (0-5, 6-14, 15-19, 20-44, 45-64, 65+)
+        [Route("rangoEtario")]
+        [HttpGet]
+        public async Task<ActionResult<int[]>> GetReporteRangoEtario()
+
+        {
+            var pacientes = await _pacientenService.List();
+            var reporteRangoEtario = new int[6];
+            reporteRangoEtario[0] = pacientes.Where(p => p.Edad < 6).Count();
+            reporteRangoEtario[1] = pacientes.Where(p => p.Edad > 5 && p.Edad < 15).Count();
+            reporteRangoEtario[2] = pacientes.Where(p => p.Edad > 14 && p.Edad < 20).Count();
+            reporteRangoEtario[3] = pacientes.Where(p => p.Edad > 19 && p.Edad < 45).Count();
+            reporteRangoEtario[4] = pacientes.Where(p => p.Edad > 44 && p.Edad < 65).Count();
+            reporteRangoEtario[5] = pacientes.Where(p => p.Edad > 64).Count();
+
+            return Ok(reporteRangoEtario);
         }
     }
 }
