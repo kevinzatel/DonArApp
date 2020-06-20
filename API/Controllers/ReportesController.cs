cabc1bd57f2a2dc750d052f42184aa5985532302
Kevin Zatel <Kevin Zatel@SVARLP2018460>
2020-06-20 13:54:56 -0300

    ReportesController agregado / voluntarios

---
+++ API/Controllers/ReportesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dto;
+using Application.Voluntarios;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportesController : ControllerBase
+    {
+        private readonly IVoluntarioBasicoService _voluntarioService;
+        private readonly IVoluntarioMedicoService _voluntarioMedicoService;
+        private readonly IVoluntarioAsociacionService _voluntarioAsociacionService;
+
+        public ReportesController(IVoluntarioBasicoService voluntarioService, IVoluntarioMedicoService voluntarioMedicoService, IVoluntarioAsociacionService voluntarioAsociacionService)
+        {
+            _voluntarioService = voluntarioService;
+            _voluntarioMedicoService = voluntarioMedicoService;
+            _voluntarioAsociacionService = voluntarioAsociacionService;
+        }
+
+        [Route("voluntarios")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VoluntarioBasico>>> Get()
+        
+        {
+            var reporteVoluntarios = new ReporteVoluntarios();
+            var voluntariosBasicos = await _voluntarioService.List();
+            var voluntariosMedicos = await _voluntarioMedicoService.List();
+            var voluntariosAsociacion = await _voluntarioAsociacionService.List();
+            reporteVoluntarios.CantVoluntariosBasicos = voluntariosBasicos.Count();
+            reporteVoluntarios.CantVoluntariosMedicos = voluntariosMedicos.Count();
+            reporteVoluntarios.CantVoluntariosAsociacion = voluntariosAsociacion.Count();
+            return Ok(reporteVoluntarios);
+        }
+    }
+}
---
+++ API/Dto/reporteVoluntarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dto
+{
+    public class ReporteVoluntarios
+    {
+        public int CantVoluntariosBasicos { get; set; }
+        public int CantVoluntariosMedicos { get; set; }
+        public int CantVoluntariosAsociacion { get; set; }
+    }
+}
